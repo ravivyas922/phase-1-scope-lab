df620217f80472c2fbbc4d49ce555cead94960e8
Ravi <ravivyas922@gmail.com>
2024-03-25 17:23:13 -0400

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+customerName = `bob`; 
+
+function upperCaseCustomerName()
+{
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer()
+{
+    bestCustomer =  `not bob`;
+}
+
+function overwriteBestCustomer()
+{
+    bestCustomer = `maybe bob`;
+}
+
+const leastFavoriteCustomer = "Me";
+
+function changeLeastFavoriteCustomer()
+{
+    leastFavoriteCustomer = "Not Me";
+}
+
+
+
